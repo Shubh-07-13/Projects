732bf8491e3906a84635f99bc2b719833b9811d1
Shubh-07-13 <shubhchouhan.aj@gmail.com>
2026-05-11 19:10:14 -0400

    Add files via upload

---
+++ arithmetic_functions.py
@@ -0,0 +1,23 @@
+def addition(x,y):
+    print(x + y)
+addition(67, 41)
+
+
+def subtraction(x,y):
+    print(x - y)
+subtraction(67, 41)
+
+def multiplication(x, y):
+    print(x * y)
+multiplication(67, 41)
+
+def division(x,y):
+    print(x / y)
+division(90, 15)
+
+z = int(input("Enter a number: "))
+t = int(input("Enter another number: "))
+addition(z, t)
+subtraction(z, t)
+multiplication(z, t)
+division(z, t)
---
+++ function_name.py
@@ -0,0 +1,4 @@
+def intro(name):
+    print("Hello! I am", name)
+user_name = input("What is your name? ")
+intro(user_name)
